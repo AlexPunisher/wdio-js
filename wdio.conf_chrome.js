8e13c0413ed7345c3595c7b2ca02aaaa288a8f30
Alex <oleksandr.shevchuk94@gmail.com>
2019-09-03 00:44:20 +0300

    1

--- wdio.conf_chrome.js
+++ wdio.conf_chrome.js
@@ -20,7 +20,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        'D:/wdio/specs/main_spec.js'
+        'D:/wdio/wdio-js/specs/main_spec.js'
     ],
     // Patterns to exclude.
     exclude: [
